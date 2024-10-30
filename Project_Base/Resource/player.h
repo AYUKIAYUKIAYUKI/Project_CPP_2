f56834898824f0489b4ee734e313a34410cf8d40
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2024-10-30 16:35:01 +0900

    ダメージ状態用のステートを用意しましたが、遷移はしません。

--- Project_Base/Resource/player.h
+++ Project_Base/Resource/player.h
@@ -40,10 +40,11 @@ namespace player
 		CPlayer();	// デフォルトコンストラクタ
 		~CPlayer();	// デストラクタ
 
-		HRESULT Init() override;	// 初期設定
-		void	Uninit() override;	// 終了設定
-		void	Update() override;	// 更新処理
-		void	Draw() override;	// 描画処理
+		HRESULT Init() override;		// 初期設定
+		void	Uninit() override;		// 終了設定
+		void	Update() override;		// 更新処理
+		void	Draw() override;		// 描画処理
+		void	To_Damage(int nDamage);	// ダメージ状態へ
 
 		const int& GetLife() const;	// 体力を取得
 		void SetLife(int nLife);	// 体力を設定
---
+++ Project_Base/Resource/player_state_damage.h
@@ -0,0 +1,38 @@
+//============================================================================
+//
+// プレイヤーダメージステート、ヘッダファイル [player_state_damage.h]
+// Author : 福田歩希
+//
+//============================================================================
+
+#ifndef _PLAYER_STATE_DAMAGE_
+#define _PLAYER_STATE_DAMAGE_	// 二重インクルード防止
+
+//****************************************************
+// インクルードファイル
+//****************************************************
+#include "player_state.h"
+
+//****************************************************
+// プレイヤー	ダメージステートクラス
+//****************************************************
+class CPlayer_State_Damage : public CPlayer_State
+{
+public:
+
+	CPlayer_State_Damage();				// デフォルトコンストラクタ
+	~CPlayer_State_Damage() override;	// デストラクタ
+
+	void Update() override;	// 更新処理
+
+private:
+
+	/// <summary> ダメージ継続期間 </summary>
+	static constexpr int MAX_DASH_DURATION = 20;
+
+	int m_nDamageDuration;	// ダメージ継続期間
+
+	void To_Default();	// 元に戻る
+};
+
+#endif	// _PLAYER_STATE_DAMAGE_
