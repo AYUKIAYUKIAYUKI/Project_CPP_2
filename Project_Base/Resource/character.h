8a04f2aa4486dd57ffedbb9e59a12d983a9ff2b7
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2024-11-01 11:00:39 +0900

    プレイヤーやエネミーなどの共通処理をまとめるためにキャラクタークラスを追加しました。

---
+++ Project_Base/Resource/character.h
@@ -0,0 +1,59 @@
+//============================================================================
+//
+// キャラクター、ヘッダファイル [character.h]
+// Author : 福田歩希
+//
+//============================================================================
+
+#ifndef _CHARACTER_H_
+#define _CHARACTER_H_	// 二重インクルード防止
+
+//****************************************************
+// インクルードファイル
+//****************************************************
+#include "object_X.h"
+
+//****************************************************
+// キャラクタークラス
+//****************************************************
+class CCharacter : public CObject_X
+{
+public:
+
+	CCharacter();			// デフォルトコンストラクタ
+	~CCharacter()override;	// デストラクタ
+
+	HRESULT Init() override;	// 初期設定
+	void	Uninit() override;	// 終了設定
+	void	Update() override;	// 更新処理
+	void	Draw() override;	// 描画処理
+
+	const float& GetDirection() const;		// 方角を取得
+	void SetDirection(float fDirection);	// 方角を設定
+
+	const float& GetMoveSpeed() const;		// 移動速度を取得
+	void SetMoveSpeed(float fMoveSpeed);	// 移動速度を設定
+
+	const D3DXVECTOR3& GetPosTarget() const;	// 目標座標を取得
+	void SetPosTarget(D3DXVECTOR3 PosTarget);	// 目標座標を設定
+
+	const D3DXVECTOR3& GetRotTarget() const;	// 目標向きを取得
+	void SetRotTarget(D3DXVECTOR3 RotTarget);	// 目標向きを設定
+
+	const int& GetLife() const;	// 体力を取得
+	void SetLife(int nLife);	// 体力を設定
+
+private:
+
+	void CorrectToTarget();	// 目標値への補正
+	void AdjustLife();		// 体力の調整
+
+	float		m_fCorrectionCoef;	// 補正係数
+	float		m_fDirection;		// 方角
+	float		m_fMoveSpeed;		// 移動速度
+	D3DXVECTOR3	m_PosTarget;		// 目標座標
+	D3DXVECTOR3	m_RotTarget;		// 目標向き
+	int			m_nLife;			// 体力
+};
+
+#endif // _PLAYER_H_
